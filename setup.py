b14a03f7feaea29a45186f6dda739a372f34bafd
David Higgins <dhiggins@spotxchange.com>
2016-07-26 14:25:18 -0600

    Fixed structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dynamic_databases',
+    version=__import__('dynamic_databases').__version__,
+    description=(
+        'Django App that allows for the addition of databases '
+        'on the fly and also the ability to query the database'
+        '\'s tables through dynamically generated models. '
+    ),
+    author='David',
+    author_email='sligodave@gmail.com',
+    url='https://github.com/sligodave/dynamic_databases/',
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
