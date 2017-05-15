20655cf6001680d88d936e3b7b688e206eb23f40
David Higgins <dhiggins@spotxchange.com>
2017-05-15 00:18:54 -0600

    Per Issue #1: - Added support for python 3 (tested with 3.6.1) - Broke dynamic database functionality out of the model and into it's own class.   The model is a method to store a config for a DynamicDatabase instance but you   no longer need a model instance to get a dynamic database instance.   You can instantiate it yourself with a name and database config dictionary,   identical to what you set in the settings.py file.

--- dynamic_databases/receivers.py
+++ dynamic_databases/receivers.py
@@ -4,13 +4,13 @@ from logging import getLogger
 from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
 
-from dynamic_databases.models import Database
+from dynamic_databases.models import DynamicDatabaseConfig
 
 
 logger = getLogger('dynamic_databases.receivers')
 
 
-@receiver([pre_save, pre_delete], sender=Database)
+@receiver([pre_save, pre_delete], sender=DynamicDatabaseConfig)
 def unregister_database(sender, **kwargs):
     if 'instance' in kwargs and kwargs['instance'].pk is not None:
         kwargs['instance'].unregister()
